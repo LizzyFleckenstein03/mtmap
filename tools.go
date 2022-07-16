76e29c6887456b623295f6309133dfb51fab12a6
Riley <riley@e926.de>
2022-07-17 01:08:41 +0200

    renamed MapBlk.PickNode to MapBlk.PeekNode

--- tools.go
+++ tools.go
@@ -4,7 +4,7 @@ import (
 	"github.com/anon55555/mt"
 )
 
-func (mb MapBlk) PickNode(i int) mt.Node {
+func (mb *MapBlk) PeekNode(i int) mt.Node {
 	return mt.Node{
 		Param0: mb.Param0[i],
 		Param1: mb.Param1[i],
